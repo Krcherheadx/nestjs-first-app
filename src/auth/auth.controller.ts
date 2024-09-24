bb79f0bded7c56e7f409ad239a5e2578cae50ec2
Krcher <krcherheadx@Hishams-MacBook-Pro.local>
2024-09-24 14:32:41 +0300

    second initilaization

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
-  signup() {}
+  signup(@Body() body: AuthDto) {
+    console.log({
+        dto:body,
+      emailType: typeof body.email,
+      passwordType: typeof body.password,
+    });
+    return {
+      email: body.email,
+      password: body.password,
+    };
+    // return this.authService.signup(body);
+  }
   @Post('signin')
-  signin() {}
+  signin() {
+    return { name: 'هلا وغلا❤️ ' };
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,13 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable({})
+import { Body, Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2'
+@Injectable()
 export class AuthService {
- 
+  constructor(private prisma: PrismaService) {}
+  signup(body: AuthDto) {
+    return { message: 'you have sent these data: ', data: body };
+  }
+  signin() {}
 }
