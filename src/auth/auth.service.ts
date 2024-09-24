27384d69799bd8cd57516946828c85ead47a9104
Krcher <krcherheadx@Hishams-MacBook-Pro.local>
2024-09-24 14:33:43 +0300

    m

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Body, Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-import * as argon from 'argon2'
+
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
